e8795044b57172d8a0b50ec8266aa47ff409c1d0
HavenbankRP <kyanhofstra3@gmail.com>
2025-07-18 03:36:10 +0200

    Initial upload

---
+++ app.js
@@ -0,0 +1,5 @@
+
+// Hier zou je code kunnen toevoegen, bijvoorbeeld spelers ophalen van een API
+document.addEventListener('DOMContentLoaded', function() {
+    document.getElementById('a').textContent = '123'; // testwaarde
+});
